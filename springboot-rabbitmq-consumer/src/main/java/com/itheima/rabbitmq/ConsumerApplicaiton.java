09ad8ef40714b38894371bb40d22e9db422cb11e
lojoan1216 <19951216qiong>
2019-11-01 08:23:50 +0800

    2019年11月1日08:23:07

---
+++ springboot-rabbitmq-consumer/src/main/java/com/itheima/rabbitmq/ConsumerApplicaiton.java
@@ -0,0 +1,11 @@
+package com.itheima.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplicaiton {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplicaiton.class, args);
+    }
+}
